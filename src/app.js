10e8f72c97e8cfc60a4c394351771da7028c3e36
Antonio Sobral <antoniosobral@poli.ufrj.br>
2020-03-28 09:20:41 -0300

    first commit-

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ class App {
   middlewares() {
     this.app.use(
       cors({
-        origin: 'https://fila.labsobral.com.br/',
+        origin: 'https://labsobral.com.br/',
       })
     );
     this.app.use(express.json());
