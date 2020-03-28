fb4c322825329e4422ee6bd3ba3ea1bbd86e92d0
Antonio Sobral <antoniosobral@poli.ufrj.br>
2020-03-28 11:36:28 -0300

    deploy final version

--- src/app.js
+++ src/app.js
@@ -7,32 +7,11 @@ import io from 'socket.io';
 import routes from './routes';
 import './database';
 
-const fs = require('fs');
-
 class App {
   constructor() {
     this.app = express();
 
-    this.privateKey = fs.readFileSync(
-      '/etc/letsencrypt/live/api.labsobral.com.br/privkey.pem',
-      'utf8'
-    );
-    this.certificate = fs.readFileSync(
-      '/etc/letsencrypt/live/api.labsobral.com.br/cert.pem',
-      'utf8'
-    );
-    this.ca = fs.readFileSync(
-      '/etc/letsencrypt/live/api.labsobral.com.br/chain.pem',
-      'utf8'
-    );
-
-    this.credentials = {
-      key: this.privateKey,
-      cert: this.certificate,
-      ca: this.ca,
-    };
-
-    this.server = https.Server(this.credentials, this.app);
+    this.server = http.Server(this.app);
 
     this.socket();
 
